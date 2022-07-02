77fc7b702e4cbc723d756cf87c130c455eb0c02b
Jayant Biswas <jayant.biswas@bobst.com>
2022-07-02 14:53:23 +0200

    All methods implemented

--- directedgraph.py
+++ directedgraph.py
@@ -1,7 +1,17 @@
 class DirectedGraph:
     
     def __init__(self, vertices, edges):
-        pass
+        self.edges = edges
+        self.vertices = vertices
 
     def get_number_of_edges(self):
-        return 0
+        return len(self.edges)
+
+    def get_number_of_vertices(self):
+        return len(self.vertices)
+    
+    def get_indegrees(self, vertex):
+        return len(list(filter(lambda x: x[1] == vertex, self.edges)))
+    
+    def get_outdegrees(self, vertex):
+        return len(list(filter(lambda x: x[0] == vertex, self.edges)))
--- test_directedgraph.py
+++ test_directedgraph.py
@@ -1,4 +1,5 @@
 import unittest
+import pickle
 
 from directedgraph import DirectedGraph
 
@@ -6,7 +7,33 @@ class DirectedGraphTest(unittest.TestCase):
 
     def test_empty_graph_returns_zero_edges(self):
         graph = DirectedGraph([], [])
-        self.assertEqual(0, graph.get_number_of_edges())
+        self.assertEquals(0, graph.get_number_of_edges())
+
+    def test_graph_with_one_edge(self):
+        graph = DirectedGraph([1, 2], [(1,2)])
+        self.assertEquals(1, graph.get_number_of_edges())
+        self.assertEquals(2, graph.get_number_of_vertices())
+        
+    def test_get_indegrees_for_graph_with_one_edge(self):
+        graph = DirectedGraph([1, 2], [(1,2)])
+        self.assertEquals(0, graph.get_indegrees(1))
+        self.assertEquals(1, graph.get_indegrees(2))
+    
+    def test_get_outdegrees_for_graph_with_one_edge(self):
+        graph = DirectedGraph([1, 2], [(1,2)])
+        self.assertEquals(1, graph.get_outdegrees(1))
+        self.assertEquals(0, graph.get_outdegrees(2))
+
+    def test_serialize_deserialize(self):
+        original_graph = DirectedGraph([1,2], [(1,2)])
+        backup = pickle.dumps(original_graph)
+        recovered_graph = pickle.loads(backup)
+        self.assertEquals(1, recovered_graph.get_number_of_edges())
+        self.assertEquals(2, recovered_graph.get_number_of_vertices())
+        self.assertEquals(0, recovered_graph.get_indegrees(1))
+        self.assertEquals(1, recovered_graph.get_indegrees(2))
+        self.assertEquals(1, recovered_graph.get_outdegrees(1))
+        self.assertEquals(0, recovered_graph.get_outdegrees(2))
 
 
 if __name__ == "__main__":
